99a9dfdd46f631a33ab33e3acbe8ab23e3ab769e
cosorio16 <carlososorio1230@gmail.com>
2023-08-08 22:25:53 -0500

    deploy portfolio

---
+++ script.js
@@ -0,0 +1,46 @@
+const projectButton = document.getElementById('projectID');
+const techToggle = document.getElementById('techsID');
+const buttonToggle = document.getElementById('buttonID');
+
+projectButton.addEventListener('click', () => {
+    techToggle.classList.toggle('hide')
+    techToggle.style.transition = '1s'
+    buttonToggle.style.transition = '1s'
+});
+
+const projectButton2 = document.getElementById('projectID2');
+const techToggle2 = document.getElementById('techsID2');
+const buttonToggle2 = document.getElementById('buttonID2');
+
+projectButton2.addEventListener('click', () => {
+    techToggle2.classList.toggle('hide');
+    techToggle2.style.transition = '1s'
+});
+
+const projectButton3 = document.getElementById('projectID3');
+const techToggle3 = document.getElementById('techsID3');
+const buttonToggle3 = document.getElementById('buttonID3');
+
+projectButton3.addEventListener('click', () => {
+    techToggle3.classList.toggle('hide');
+    techToggle3.style.transition = '1s'
+});
+
+const projectButton4 = document.getElementById('projectID4');
+const techToggle4 = document.getElementById('techsID4');
+const buttonToggle4 = document.getElementById('buttonID4');
+
+projectButton4.addEventListener('click', () => {
+    techToggle4.classList.toggle('hide');
+    techToggle4.style.transition = '1s'
+});
+
+
+const projectButton5 = document.getElementById('projectID5');
+const techToggle5 = document.getElementById('techsID5');
+const buttonToggle5 = document.getElementById('buttonID5');
+
+projectButton5.addEventListener('click', () => {
+    techToggle5.classList.toggle('hide');
+    techToggle5.style.transition = '1s'
+});
